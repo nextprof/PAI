089ff2c49f1bf64e77ded971cace5f14343cb19d
nextprof <kubabednarowicz8@gmail.com>
2022-01-27 20:17:41 +0100

    Init from PAI Lab

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+require 'Routing.php';
+
+$path = trim($_SERVER['REQUEST_URI'], '/');
+$path = parse_url($path, PHP_URL_PATH);
+
+Router::get('', 'DefaultController');
+Router::get('projects', 'ProjectController');
+Router::post('login', 'SecurityController');
+Router::post('addProject', 'ProjectController');
+Router::post('register', 'SecurityController');
+Router::post('search', 'ProjectController');
+Router::get('like', 'ProjectController');
+Router::get('dislike', 'ProjectController');
+
+Router::run($path);
---
+++ src/models/User.php
@@ -0,0 +1,61 @@
+<?php
+
+class User {
+    private $email;
+    private $password;
+    private $name;
+    private $surname;
+    private $phone;
+
+    public function __construct(
+        string $email,
+        string $password,
+        string $name,
+        string $surname
+    ) {
+        $this->email = $email;
+        $this->password = $password;
+        $this->name = $name;
+        $this->surname = $surname;
+    }
+
+    public function getEmail(): string 
+    {
+        return $this->email;
+    }
+
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    public function getSurname(): string
+    {
+        return $this->surname;
+    }
+
+    public function setSurname(string $surname): void
+    {
+        $this->surname = $surname;
+    }
+
+    public function getPhone()
+    {
+        return $this->phone;
+    }
+
+    public function setPhone($phone): void
+    {
+        $this->phone = $phone;
+    }
+}
