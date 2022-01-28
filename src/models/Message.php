eb0cd0a5f287e9d6e4c097680459d60f2edd03d8
nextprof <kubabednarowicz8@gmail.com>
2022-01-28 21:54:09 +0100

    Messages

---
+++ src/models/Message.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace models;
+
+class Message
+{
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     */
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return int
+     */
+    public function getIdFrom(): int
+    {
+        return $this->id_from;
+    }
+
+    /**
+     * @param int $id_from
+     */
+    public function setIdFrom(int $id_from): void
+    {
+        $this->id_from = $id_from;
+    }
+
+    /**
+     * @return int
+     */
+    public function getIdTo(): int
+    {
+        return $this->id_to;
+    }
+
+    /**
+     * @param int $id_to
+     */
+    public function setIdTo(int $id_to): void
+    {
+        $this->id_to = $id_to;
+    }
+
+    /**
+     * @return string
+     */
+    public function getMessage(): string
+    {
+        return $this->message;
+    }
+
+    /**
+     * @param string $message
+     */
+    public function setMessage(string $message): void
+    {
+        $this->message = $message;
+    }
+    private int $id;
+    private int $id_from;
+    private int $id_to;
+    private string $message;
+
+
+}
