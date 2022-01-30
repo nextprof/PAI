d2c04742478318d2cde8f488f8508b21231ef477
nextprof <kubabednarowicz8@gmail.com>
2022-01-30 01:26:44 +0100

    Login, register links

--- public/views/login.php
+++ public/views/login.php
@@ -6,25 +6,27 @@
 </head>
 
 <body>
-    <div class="container">
-        <div class="logo">
-            <img src="public/img/logo.png">
-        </div>
-        <div class="login-container">
-            <form class="login" action="login" method="POST">
-                <div class="messages">
-                    <?php
-                        if(isset($messages)){
-                            foreach($messages as $message) {
-                                echo $message;
-                            }
-                        }
-                    ?>
-                </div>
-                <input name="email" type="email" placeholder="email@email.com">
-                <input name="password" type="password" placeholder="password">
-                <button type="submit">LOGIN</button>
-            </form>
-        </div>
+<div class="container">
+    <div class="logo">
+        <img src="public/img/logo.png">
     </div>
+    <div class="login-container">
+        <form class="login" action="login" method="POST">
+            <div class="messages">
+                <?php
+                if (isset($messages)) {
+                    foreach ($messages as $message) {
+                        echo $message;
+                    }
+                }
+                ?>
+            </div>
+            <input name="email" type="email" placeholder="email@email.com">
+            <input name="password" type="password" placeholder="password">
+            <button type="submit">LOGIN</button>
+            <a class="container-link" href="/register">Create account</a>
+        </form>
+
+    </div>
+</div>
 </body>
--- public/views/register.php
+++ public/views/register.php
@@ -7,27 +7,29 @@
 </head>
 
 <body>
-    <div class="container">
-        <div class="logo">
-            <img src="public/img/logo.png">
-        </div>
-        <div class="login-container">
-            <form class="register" action="register" method="POST">
-                <div class="messages">
-                    <?php
-                        if(isset($messages)){
-                            foreach($messages as $message) {
-                                echo $message;
-                            }
-                        }
-                    ?>
-                </div>
-                <input name="email" type="text" placeholder="email@email.com">
-                <input name="password" type="password" placeholder="password">
-                <input name="confirmedPassword" type="password" placeholder="confirm password">
-                <input name="username" type="text" placeholder="username">
-                <button type="submit">REGISTER</button>
-            </form>
-        </div>
+<div class="container">
+    <div class="logo">
+        <img src="public/img/logo.png">
     </div>
+    <div class="login-container">
+        <form class="register" action="register" method="POST">
+            <div class="messages">
+                <?php
+                if (isset($messages)) {
+                    foreach ($messages as $message) {
+                        echo $message;
+                    }
+                }
+                ?>
+            </div>
+            <input name="email" type="text" placeholder="email@email.com">
+            <input name="password" type="password" placeholder="password">
+            <input name="confirmedPassword" type="password" placeholder="confirm password">
+            <input name="username" type="text" placeholder="username">
+            <button type="submit">REGISTER</button>
+            <a class="container-link" href="/login">Already have account</a>
+        </form>
+
+    </div>
+</div>
 </body>
